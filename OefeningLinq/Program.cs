5ef7aab834074ff08a182d6ca35f48463cac21e9
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-22 11:12:09 +0100

    Oplossingen Oefeningen Personen Linq

--- OefeningLinq/Program.cs
+++ OefeningLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace OefeningLinq
 {
     enum Geslacht
@@ -24,10 +25,32 @@ namespace OefeningLinq
                 new Persoon(){ Naam="Jos" , Geslacht = Geslacht.X,GeboorteDatum = new DateTime(1983,10,1)},
             };
             //Oefening1: Maak een Linq query (zelf syntax kiezen), die alle namen van Personen selecteert die
-           // beginnen met de letter 'j' (case-insensitive , zowel hoofd-als kleine letters) 
-           //sorteer op Geboortedatum
-           //Oefening 2: Maak een Linq query , die het Geslacht en Naam teruggeeft van de
-           // de personen die in januari geboren zijn teruggeeft. Geef daarna ook het aantal Personen terug die in januari geboren.
+            // beginnen met de letter 'j' (case-insensitive , zowel hoofd-als kleine letters) 
+            //sorteer op Geboortedatum
+            var namen = from p in personen
+                        where p.Naam.Contains("j", StringComparison.OrdinalIgnoreCase)
+                        orderby p.GeboorteDatum
+                        select p.Naam;
+            List<string> lijstNamen = namen.ToList();
+            lijstNamen.ForEach(n => Console.WriteLine(n));
+            //Methode notatie:
+            var resultPersonenMetJ = personen.Where(p => p.Naam.Contains("j", StringComparison.OrdinalIgnoreCase))
+                .OrderBy(p => p.GeboorteDatum).Select(p => p.Naam);
+            //Oefening 2: Maak een Linq query , die het Geslacht en Naam teruggeeft van de
+            //de personen die in januari geboren zijn. Geef daarna ook het aantal Personen terug die 
+            //in januari geboren.
+            var personenGeborenInJan = personen.Where(p => p.GeboorteDatum.Month == 1)
+                                                .Select(p => new
+                                                            {
+                                                                Sex = p.Geslacht,
+                                                                Name = p.Naam
+                                                            });
+            Console.WriteLine("Het aantal personen geboren in Januari: " + personenGeborenInJan.Count());
+            foreach (var el in personenGeborenInJan)
+            {
+                Console.WriteLine(el.Name + ":" + el.Sex);
+            }
+
         }
     }
 }
