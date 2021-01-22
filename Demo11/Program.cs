9ca3849ad8a111484d40fded589c063324795f48
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-22 12:34:13 +0100

    Demo 11 - First, Single, FirstOrDefault, SingleOrDefault

--- Demo11/Program.cs
+++ Demo11/Program.cs
@@ -24,7 +24,7 @@ namespace Demo11
         public static List<Product> ProductsList { get; } = new List<Product>() {
         new Product { ProductID = 1, ProductName = "Chai",Category = "Beverages", UnitPrice = 18.0000M, UnitsInStock = 39 },
         new Product { ProductID = 2, ProductName = "Chang",Category = "Beverages", UnitPrice = 19.0000M, UnitsInStock = 17 },
-        new Product { ProductID=3, ProductName ="Aniseed Syrup",Category="Condiments",UnitPrice=10.0000M, UnitsInStock=13 },
+        new Product { ProductID = 3, ProductName ="Aniseed Syrup",Category="Condiments",UnitPrice=10.0000M, UnitsInStock=13 },
         new Product { ProductID = 4, ProductName="Chef Anton's Cajun Seasoning",Category= "Condiments", UnitPrice = 22.0000M, UnitsInStock = 53 },
         new Product { ProductID = 5, ProductName = "Chef Anton's Gumbo Mix", Category = "Condiments", UnitPrice = 21.3500M, UnitsInStock = 0 }};
     }
@@ -34,6 +34,35 @@ namespace Demo11
         static void Main(string[] args)
         {
             List<Product> productenLijst = Products.ProductsList;
+            Product productId3 = (from p in productenLijst
+                                  where p.ProductID == 3
+                                  select p).First();
+
+            Console.WriteLine($"Het product met Id==3 : \n {productId3}");
+
+            Product productId6 = (from p in productenLijst
+                                  where p.ProductID == 6
+                                  select p).FirstOrDefault();
+            if (productId6 == null)
+                Console.WriteLine($"Het product met Id==6 niet gevonden");
+            else
+                Console.WriteLine($"Het product met Id==6 : \n {productId6}");
+
+
+            Product productBeveragesFirst = productenLijst.Where(p => p.Category == "Beverages").First();
+            Console.WriteLine($"Het eerste product met Categorie='Beverages' : \n {productBeveragesFirst}");
+
+            Product productBeveragesSingle = productenLijst.Where(p => p.Category == "Beverages").Single();
+            //geef exception, want meer dan 1 product van categorie 'Beverages'
+            Console.WriteLine($"Het enige productmet Categorie='Beverages' : \n {productBeveragesSingle}");
+
+
+
+            Product productId6Single = productenLijst.Where(p => p.ProductID == 6).SingleOrDefault();
+            if (productId6Single == null)
+                Console.WriteLine($"Het product met Id==6 niet gevonden");
+            else
+                Console.WriteLine($"Het product met Id==6 : \n {productId6Single}");
         }
     }
 
