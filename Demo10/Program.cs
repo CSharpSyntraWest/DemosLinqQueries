69ee3688f2e3c577c09752850b6319fcb10d1417
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-22 11:34:45 +0100

    Demo Aggregaat methodes bij Linq

---
+++ Demo10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace Demo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aggregation methoden:
+            //Count(), Sum(),Average(), Max() en Min()
+
+            double[] temperaturen = { 28.0, 19.5, 32.3, 33.6, 26.5, 29.7 };
+            int aantalTempBoven30 = temperaturen.Where(t => t > 30).Count();
+            int aantalTempBoven30b = temperaturen.Count(t=>t>30);
+            //Query Expr notatie:
+            int aantalTempBoven30c = (from t in temperaturen
+                                      where t > 30
+                                      select t).Count();
+
+            int aantalTempBoven30d = (from t in temperaturen
+                                      select t).Count(t => t > 30);
+
+            //De som van alle temperaturen hoger dan 26:
+            double som = temperaturen.Where(t => t>26).Sum();
+            double gemiddelde = temperaturen.Average();
+            double maxTemp = temperaturen.Max();
+            double minTemp = temperaturen.Min();
+        }
+    }
+}
---
+++ Demo9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "Aantal 12 niet-letters 5 in deze 1.0 tekst: 8";
+            int aantalNietAlfChars =
+                tekst.Where(c => !Char.IsLetter(c))
+                     .Count();
+            Console.WriteLine("Aantal niet-alf karakters=" + aantalNietAlfChars);
+            //Zelfde Linq query met Query Expression syntax:
+            var aantalNietAlfChars2 =
+                (from c in tekst
+                 where !Char.IsLetter(c)
+                 select c).Count();
+            //Oefening: Geef eens het aantal Digits in de tekst via een linq query
+            var aantalDigits =
+                   tekst.Where(c => Char.IsDigit(c))
+                   .Count();
+            var aantalDigits2 =
+                (from c in tekst
+                 where Char.IsDigit(c)
+                 select c).Count();
+            Console.WriteLine("Aantal digit-karakters=" + aantalDigits2);
+
+        }
+    }
+}
